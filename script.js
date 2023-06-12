4a7ac1e33aebcfca7c40dc71368b473d930f8608
Harikakashraju <125797836+Harikakashraju@users.noreply.github.com>
2023-06-12 14:04:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 //your code here
-touristSpots = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-touristSpots.sort();
-console.log(touristSpots);
+function sortAndDisplayBandNames(bandNames) {
+  const articles = ['a', 'an', 'the'];
+
+  // Remove articles from band names and create a new array
+  const bandNamesWithoutArticles = bandNames.map((name) => {
+    const words = name.split(' ');
+    const filteredWords = words.filter((word) => !articles.includes(word.toLowerCase()));
+    return filteredWords.join(' ');
+  });
+
+  // Sort band names in lexicographic order
+  const sortedBandNames = bandNamesWithoutArticles.sort((a, b) => a.localeCompare(b));
+
+  // Display band names in a UL list with LI tags
+  const ulElement = document.getElementById('band');
+  sortedBandNames.forEach((name) => {
+    const liElement = document.createElement('li');
+    liElement.textContent = name;
+    ulElement.appendChild(liElement);
+  });
+}
+
+// Example usage:
+const bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+sortAndDisplayBandNames(bandNames);
+
